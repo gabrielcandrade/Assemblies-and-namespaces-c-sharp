4ae95011150c84d8146cce9c7f16b06f93b2f6e7
Gabriel Andrade <gabrielcandrade01@gmail.com>
2018-02-28 13:53:49 -0300

    Try

---
+++ AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+
+            Console.WriteLine(reply);
+            File.WriteAllText(@"C:\Users\PrinceBOSS\Desktop\Gabriel Andrade\WriteText.txt", reply);
+            Console.ReadLine();
+        }
+    }
+}
